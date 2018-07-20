a67efb2dd23b53ce10defeff9430b30d92d31729
gunturkh <guntur.kh@gmail.com>
2018-07-20 15:37:54 +0700

    Adding multiline comment Adding mystring1

--- javascript-project/manipulation-string/level-4-5-6-7/index.js
+++ javascript-project/manipulation-string/level-4-5-6-7/index.js
@@ -1,6 +1,13 @@
 var mystring = "Hello World";
+var mystring1 = "What's Up?";
 
 console.log(mystring.charAt(4)); //return o as result
 console.log(mystring.charCodeAt(4)); //return '111' as Unicode of 'o' 
 console.log(mystring.toUpperCase()); //return HELLO WORLD
 console.log(mystring.toLowerCase()); // return hello world
+
+console.log(mystring + ", " +  mystring1);
+/* Add mystring and 
+* mystring1 together 
+ with ", " in between
+ */
