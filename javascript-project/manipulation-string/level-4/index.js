69fc6e53ce02698ba97f872a2d98fa3c2034901b
gunturkh <guntur.kh@gmail.com>
2018-07-20 15:14:27 +0700

    Adding manipulating-string folder
    Removing comment on level-3 index.js

---
+++ javascript-project/manipulation-string/level-4/index.js
@@ -0,0 +1,27 @@
+var numberA = ["H","E","L","L","O"];
+
+
+
+
+
+var result = numberA.toString(); 
+console.log(result);
+
+var result = numberA.join("_"); 
+console.log(result);
+
+var result = numberA.length; 
+console.log(result);
+
+var result = numberA.includes("E"); 
+console.log(result);
+
+var result = numberA.push("!"); 
+console.log(result);
+
+var result = numberA.pop(); 
+console.log(result);
+
+
+
+
