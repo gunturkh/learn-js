1b476b54338d9f9172554120dcb434ee4ddd24cc
gunturkh <guntur.kh@gmail.com>
2018-07-22 22:16:42 +0700

    Updating level-8 and level-9 index.js

--- javascript-project/comparison-number/index.js
+++ javascript-project/comparison-number/index.js
@@ -1,5 +1,20 @@
 var num = 11;
 var num1 = 12;
+var num2 = 13;
+var numdefault=10;
+
+if (num2>numdefault) {
+    console.log(`${num2} is smaller than determined value : ${numdefault}`)
+}
+
+else if (num2==numdefault) {
+    console.log(`${num2} is equal with determined value : ${numdefault}`)
+}
+
+else {
+    console.log(`${num2} is bigger than determined value : ${numdefault}`)
+}
+
 
 if (num>num1) {
     console.log(`${num} is bigger than ${num1}`);
