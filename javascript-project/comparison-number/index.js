3fb616ad9474ad9edc260d44a959f3ab0d9fc6e4
gunturkh <guntur.kh@gmail.com>
2018-07-22 23:02:30 +0700

    Add level-9-10-11 on index.js

--- javascript-project/comparison-number/index.js
+++ javascript-project/comparison-number/index.js
@@ -2,8 +2,8 @@ var num = 11;
 var num1 = 12;
 var num2 = 13;
 var numdefault=10;
-
-if (num2>numdefault) {
+//level-8
+if (num2<numdefault) {
     console.log(`${num2} is smaller than determined value : ${numdefault}`)
 }
 
@@ -15,7 +15,7 @@ else {
     console.log(`${num2} is bigger than determined value : ${numdefault}`)
 }
 
-
+//level-9
 if (num>num1) {
     console.log(`${num} is bigger than ${num1}`);
 }
@@ -25,3 +25,14 @@ else if (num<num1) {
 else {
     console.log(`${num} is equal with ${num1}`);
 }
+
+//level-10
+var conditionnum = `The number ${num} is ` + (num<num1 ? 'smaller' : 'bigger') + ` than ${num1}`;
+console.log(conditionnum)
+
+//level-11
+var comparisonnum = 3
+var conditionnum1 = 10>=comparisonnum && comparisonnum>=5
+var conditionnum2 = comparisonnum>2
+console.log(conditionnum1)
+console.log(conditionnum2)
