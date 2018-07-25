63c73d8de1f9dacc03f945d1f00478368f5b5e19
gunturkh <guntur.kh@gmail.com>
2018-07-25 15:27:04 +0700

    Delete unused variable

--- javascript-project/calculator-time/index.js
+++ javascript-project/calculator-time/index.js
@@ -1,4 +1,19 @@
-const hourForm = $("hour-form")
-const minuteForm = $("minute-form")
-const secondForm = $("second-form")
-const resultForm = $("result-form")
+const timeForm = $("time-form")
+
+
+// Time Functions
+
+const hourToMinute = (a) => Number(a * 60)
+const hourToSecond = (a) => Number(a * 3600)
+const minuteToSecond = (a) => Number(a * 60)
+const minuteToHour = (a) => Number(a / 60)
+const secondToMinute = (a) => Number(a / 60)
+const secontToHour = (a) => Number(a/3600)
+
+const addDOM = (event) => {
+    event.preventDefault()
+    const addFirst = Number(document.getElementById("add-1").value)
+    const addSecond = Number(document.getElementById("add-2").value)
+    if (addFirst && addSecond) addResult.innerHTML = add(addFirst, addSecond)
+
+}
