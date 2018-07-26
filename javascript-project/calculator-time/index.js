ec4dae79929aeff643004737fd26d584921429fa
gunturkh <guntur.kh@gmail.com>
2018-07-26 09:43:00 +0700

    Add result in minute and second method

--- javascript-project/calculator-time/index.js
+++ javascript-project/calculator-time/index.js
@@ -1,24 +1,40 @@
-const timeForm = $("time-form")
-const resultInSecondForm = $("result-in-second")
+$(function () {
+    const timeForm = $("#time-form")
+    const resultInSecondForm = $("#result-in-second-form")
+    const resultInMinuteForm = $("#result-in-minute-form")
+    const resultInHourForm = $("#result-in-hour-form")
 
 
-// Time Functions
 
-const hourToMinute = (a) => Number(a * 60)
-const hourToSecond = (a) => Number(a * 3600)
-const minuteToSecond = (a) => Number(a * 60)
-const minuteToHour = (a) => Number(a / 60)
-const secondToMinute = (a) => Number(a / 60)
-const secontToHour = (a) => Number(a/3600)
+    // Time Functions
 
-const timeDOM = (event) => {
-    event.preventDefault()
-    const hourValue = Number($("hour").value)
-    const minuteValue = Number($("minute").value)
-    const secondValue = Number($("second").value)
-    if (hourValue && minuteValue && secondValue) resultInSecondForm.innerHTML = hourToSecond(a)
+    const hourToHour = (a) => Number(a)
+    const hourToMinute = (a) => Number(a * 60)
+    const hourToSecond = (a) => Number(a * 3600)
+    const minuteToSecond = (a) => Number(a * 60)
+    const minuteToMinute = (a) => Number(a)
+    const secondToSecond = (a) => Number(a)
+    const minuteToHour = (a) => Number(a / 60)
+    const secondToMinute = (a) => Number(a / 60)
+    const secondToHour = (a) => Number(a / 3600)
 
-}
 
-// Event Listener
-timeForm.on("submit", timeDOM)
+    const timeDOM = (event) => {
+        event.preventDefault()
+        const hourInputValue = $("#hour-input").val()
+        const minuteInputValue = $("#minute-input").val()
+        const secondInputValue = $("#second-input").val()
+        resultInSecondForm.val(hourToSecond(hourInputValue) + minuteToSecond(minuteInputValue) + secondToSecond(secondInputValue))
+        resultInMinuteForm.val(hourToMinute(hourInputValue) + minuteToMinute(minuteInputValue) + secondToMinute(secondInputValue))
+        resultInHourForm.val(hourToHour(hourInputValue) + minuteToHour(minuteInputValue) + secondToHour(secondInputValue))
+
+        // const minuteValue = Number($("minute").value)
+        // const secondValue = Number($("second").value)
+        // if (hourValue && minuteValue && secondValue) 
+
+    }
+
+    // Event Listener
+    timeForm.on("submit", timeDOM)
+})
+
